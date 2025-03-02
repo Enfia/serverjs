0ec6cec9d74624ba321285abdb7c14676dad45b5
Enfia <enfiaxd@gmail.com>
2025-03-02 12:50:57 +0900

    테스트임

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const userRoutes = require('./routes/userRoutes');  // userRoutes 파일을 임�
 const connection = require('./database/db.js'); // 예시 경로
 const bodyParser = require('body-parser');
 require('dotenv').config(); // .env 파일 로드
+const v2 = "ㅁㄴㅇ";
 
 // 뷰 엔진 설정
 app.set('view engine', 'ejs');
